67195b0c674578873600ca7c6413e05a03015f02
gusttavokr <gustavommilitaoo@gmail.com>
2026-03-20 16:19:22 -0300

    :sparkles: feat: tad arvore binaria

---
+++ Interfaces/TAD_ArvoreBinaria.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+import Classes.Node;
+
+public interface TAD_ArvoreBinaria extends TAD_ArvoreGenerica {
+    public Node leftChild;
+    public Node rightChild;
+    public boolean hasLeft;
+    public boolean hasRight;
+} 
