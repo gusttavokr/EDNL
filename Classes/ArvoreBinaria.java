4e8214a21fbf7896b81d519334b2a41061ff126d
Gustavo Maia <gustavommilitao06@gmail.com>
2026-03-26 20:22:04 -0300

    :sparkles: feat: inOrder + print

--- Classes/ArvoreBinaria.java
+++ Classes/ArvoreBinaria.java
@@ -165,7 +165,7 @@ public class ArvoreBinaria extends ArvoreGenerica implements TAD_ArvoreBinaria {
         tamanho--;
         return n.get_element();
     }
-    
+
     @Override
     public int height(Node n){
         if (isEmpty()) {
@@ -198,5 +198,45 @@ public class ArvoreBinaria extends ArvoreGenerica implements TAD_ArvoreBinaria {
             array.add(rightChild(n));
         }
         return array.iterator();
+    }
+
+    public void inOrder(Node n, String[][] matriz, int colunaAtual[]){
+
+        if (hasLeft(n)) {
+            inOrder(leftChild(n), matriz, colunaAtual);
+        }
+
+        int linha = depth(n);
+        int coluna = colunaAtual[0]++;
+        matriz[linha][coluna] = n.get_element().toString();
+
+        if (hasRight(n)) {
+            inOrder(rightChild(n), matriz, colunaAtual);
+        }
+    }
+
+    public void printArvore(){
+        int linhas = height(raiz) + 1;
+        int colunas = size();
+        String[][] matriz = new String[linhas][colunas];
+
+        for(int i = 0; i < linhas; i++){
+            for (int j = 0; j < colunas; j++){
+                matriz[i][j] = " ";
+            }
+        }
+        
+        int colunaAtual[] = new int[1];
+        colunaAtual[0] = 0;
+        
+        inOrder(raiz, matriz, colunaAtual);
+        
+        for(int i = 0; i < linhas; i++){
+            for (int j = 0; j < colunas; j++){
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
