c48543de5089349065b11712fff7fdec62ba27c2
Gustavo Maia <gustavommilitao06@gmail.com>
2026-03-26 20:50:30 -0300

    :white_check_mark: test: arvore binaria

---
+++ Tests/TesteArvoreBinaria.java
@@ -0,0 +1,26 @@
+package Tests;
+
+import Classes.ArvoreBinaria;
+import Classes.Node;
+
+public class TesteArvoreBinaria {
+    public static void main(String[] args) {
+        
+        ArvoreBinaria arvore = new ArvoreBinaria("1");
+
+        Node raiz = arvore.root();
+
+        arvore.insertLeft("2", raiz);
+        arvore.insertRight("3", raiz);
+
+        Node filho1 = arvore.leftChild(raiz);
+        Node filho2 = arvore.rightChild(raiz);
+
+        arvore.printArvore();
+
+        System.out.println("Removendo o 2");
+        
+        arvore.remove(filho1);
+        arvore.printArvore();
+    }
+}
