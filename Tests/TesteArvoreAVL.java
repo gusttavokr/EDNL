ffe7e0c9c7423d805324b6dc92a092d88f404d67
gusttavokr <gustavommilitaoo@gmail.com>
2026-04-06 15:34:13 -0300

    :white_check_mark: test: inserção

--- Tests/TesteArvoreAVL.java
+++ Tests/TesteArvoreAVL.java
@@ -6,8 +6,14 @@ public class TesteArvoreAVL {
     public static void main(String[] args) {
         System.out.println("Testes para a Arvore AVL");
 
-        ArvoreAVL arvore = new ArvoreAVL(1);
-//        arvore.insercaoAVL(2);
+        ArvoreAVL arvore = new ArvoreAVL(2);
+
+        System.out.println("Inserindo o: 3");
+        arvore.insercaoAVL(3);
+        arvore.printArvore();
+
+        System.out.println("Inserindo o: 1");
+        arvore.insercaoAVL(1);
         arvore.printArvore();
     }
 }
