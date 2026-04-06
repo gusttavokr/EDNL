0b76310f62ea72e3a4a9442ffbc3e6a303fab22f
gusttavokr <gustavommilitaoo@gmail.com>
2026-04-06 14:24:00 -0300

    :white_check_mark: test: print arvoreAVL

--- Tests/TesteArvoreAVL.java
+++ Tests/TesteArvoreAVL.java
@@ -4,6 +4,10 @@ import Classes.ArvoreAVL;
 
 public class TesteArvoreAVL {
     public static void main(String[] args) {
-        System.out.println("Testes para a Arvore Binaria");
+        System.out.println("Testes para a Arvore AVL");
+
+        ArvoreAVL arvore = new ArvoreAVL(1);
+//        arvore.insercaoAVL(2);
+        arvore.printArvore();
     }
 }
