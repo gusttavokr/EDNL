a8c59093a338bcf380e34b2059393f174950d9cf
gusttavokr <gustavommilitaoo@gmail.com>
2026-04-06 14:23:01 -0300

    :sparkles: feat: printar árvore + FB

--- Classes/ArvoreAVL.java
+++ Classes/ArvoreAVL.java
@@ -1,7 +1,50 @@
 package Classes;
 
+import Exceptions.PosicaoInvalida;
+
 public class ArvoreAVL extends ArvorePesquisa {
     public ArvoreAVL(Object o){
         super(o);
+    }
+
+
+    public void inOrder(Node n, String[][] matriz, int colunaAtual[]){
+
+        if (hasLeft(n)) {
+            inOrder(leftChild(n), matriz, colunaAtual);
+        }
+
+        int linha = depth(n);
+        int coluna = colunaAtual[0]++;
+        matriz[linha][coluna] = n.get_element().toString() + " [" + n.get_FB() + "]";
+
+        if (hasRight(n)) {
+            inOrder(rightChild(n), matriz, colunaAtual);
+        }
+    }
+
+    public void printArvore(){
+        int linhas = height(raiz) + 1;
+        int colunas = size();
+        String[][] matriz = new String[linhas][colunas];
+
+        for(int i = 0; i < linhas; i++){
+            for (int j = 0; j < colunas; j++){
+                matriz[i][j] = " ";
+            }
+        }
+
+        int colunaAtual[] = new int[1];
+        colunaAtual[0] = 0;
+
+        inOrder(raiz, matriz, colunaAtual);
+
+        for(int i = 0; i < linhas; i++){
+            for (int j = 0; j < colunas; j++){
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
