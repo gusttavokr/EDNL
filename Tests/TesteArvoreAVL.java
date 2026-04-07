2df40617059eb34252edce5d194c681acc9e2f7b
Gustavo Maia <gustavommilitao06@gmail.com>
2026-04-06 21:27:55 -0300

    :white_check_mark: test: balanceado e desbalanceado

--- Tests/TesteArvoreAVL.java
+++ Tests/TesteArvoreAVL.java
@@ -6,14 +6,36 @@ public class TesteArvoreAVL {
     public static void main(String[] args) {
         System.out.println("Testes para a Arvore AVL");
 
-        ArvoreAVL arvore = new ArvoreAVL(2);
-
-        System.out.println("Inserindo o: 3");
-        arvore.insercaoAVL(3);
+        ArvoreAVL arvore = new ArvoreAVL(3);
+        
+        
+        System.out.println("Inserindo o: 2");
+        arvore.insercaoAVL(2);
         arvore.printArvore();
 
         System.out.println("Inserindo o: 1");
         arvore.insercaoAVL(1);
         arvore.printArvore();
+
+        System.out.println("Inserindo o: 5");
+        arvore.insercaoAVL(5);
+        arvore.printArvore();
+
+        System.out.println("Inserindo o: 6");
+        arvore.insercaoAVL(6);
+        arvore.printArvore();
+
+
+        // ArvoreAVL arvore = new ArvoreAVL(1);
+
+        // System.out.println("Inserindo o: 2");
+        // arvore.insercaoAVL(2);
+        // arvore.printArvore();
+        // System.out.println("Inserindo o: 3");
+        // arvore.insercaoAVL(3);
+        // arvore.printArvore();
+        // System.out.println("Inserindo o: 4");
+        // arvore.insercaoAVL(4);
+        // arvore.printArvore();
     }
 }
