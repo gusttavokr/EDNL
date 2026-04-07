8efcc33e8d6caa04430858e9f904891343668bf5
gusttavokr <gustavommilitaoo@gmail.com>
2026-04-07 14:51:20 -0300

    :sparkles: feat: Rotação simples esquerda

--- Classes/ArvoreAVL.java
+++ Classes/ArvoreAVL.java
@@ -34,15 +34,49 @@ public class ArvoreAVL extends ArvorePesquisa {
     }
 
     public void atualizarFB(Node n, Node p){
+
+        Node desbalanceado = null;
+
         while (p != null) {
             if (comparar(n.get_element(), p.get_element()) < 0) {
                 p.set_FB(p.get_FB()+1);
                 p = p.get_pai();
             } else if (comparar(n.get_element(), p.get_element()) > 0) {
                 p.set_FB(p.get_FB()-1);
+
+                if (p.get_FB() < -1){
+                    desbalanceado = p;
+                }
+
                 p = p.get_pai();
             }
         }
+
+        if (desbalanceado != null){
+            rotacao(desbalanceado);
+        }
+    }
+
+    public void rotacao(Node n){
+
+        System.out.println("Rotacionando: " + n.get_element() + " [" + n.get_FB() + "]");
+
+        Node sucessor = n.get_filhoD();
+        Node beta = sucessor.get_filhoE();
+        Node oldPai = n.get_pai();
+
+        sucessor.set_pai(oldPai);
+        if (n == raiz){
+            raiz = sucessor;
+        }
+
+        sucessor.set_filhoE(n);
+        n.set_filhoD(beta);
+        n.set_pai(sucessor);
+
+        n.set_FB(n.get_FB() + 1 - Math.min(sucessor.get_FB(), 0));
+        sucessor.set_FB(sucessor.get_FB() + 1 + Math.max(n.get_FB(), 0));
+
     }
 
     public void inOrder(Node n, String[][] matriz, int colunaAtual[]){
@@ -78,7 +112,7 @@ public class ArvoreAVL extends ArvorePesquisa {
 
         for(int i = 0; i < linhas; i++){
             for (int j = 0; j < colunas; j++){
-                System.out.printf("%-5s", matriz[i][j]);
+                System.out.printf("%-4s", matriz[i][j]);
             }
             System.out.println();
         }
