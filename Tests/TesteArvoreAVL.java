6f2413c0b738eb13eba3487c727426dc86e1c33e
gusttavokr <gustavommilitaoo@gmail.com>
2026-04-07 14:51:59 -0300

    :white_check_mark: test: inserção com rotação simples esquerda

--- Tests/TesteArvoreAVL.java
+++ Tests/TesteArvoreAVL.java
@@ -6,36 +6,46 @@ public class TesteArvoreAVL {
     public static void main(String[] args) {
         System.out.println("Testes para a Arvore AVL");
 
-        ArvoreAVL arvore = new ArvoreAVL(3);
-        
-        
-        System.out.println("Inserindo o: 2");
-        arvore.insercaoAVL(2);
-        arvore.printArvore();
+//        ArvoreAVL arvore = new ArvoreAVL(3);
+//
+//
+//        System.out.println("Inserindo o: 2");
+//        arvore.insercaoAVL(2);
+//        arvore.printArvore();
+//
+//        System.out.println("Inserindo o: 1");
+//        arvore.insercaoAVL(1);
+//        arvore.printArvore();
+//
+//        System.out.println("Inserindo o: 5");
+//        arvore.insercaoAVL(5);
+//        arvore.printArvore();
+//
+//        System.out.println("Inserindo o: 6");
+//        arvore.insercaoAVL(6);
+//        arvore.printArvore();
 
-        System.out.println("Inserindo o: 1");
-        arvore.insercaoAVL(1);
-        arvore.printArvore();
 
-        System.out.println("Inserindo o: 5");
-        arvore.insercaoAVL(5);
-        arvore.printArvore();
+        // Desbalanceada pra direita
+         ArvoreAVL arvore = new ArvoreAVL(1);
 
-        System.out.println("Inserindo o: 6");
-        arvore.insercaoAVL(6);
-        arvore.printArvore();
+         System.out.println("Inserindo o: 2");
+         arvore.insercaoAVL(2);
+         arvore.printArvore();
+         System.out.println("Inserindo o: 3");
+         arvore.insercaoAVL(3);
+         arvore.printArvore();
 
-
-        // ArvoreAVL arvore = new ArvoreAVL(1);
-
-        // System.out.println("Inserindo o: 2");
-        // arvore.insercaoAVL(2);
-        // arvore.printArvore();
-        // System.out.println("Inserindo o: 3");
-        // arvore.insercaoAVL(3);
-        // arvore.printArvore();
-        // System.out.println("Inserindo o: 4");
-        // arvore.insercaoAVL(4);
-        // arvore.printArvore();
+//        ArvoreAVL arvore = new ArvoreAVL(10);
+//
+//        System.out.println("Inserindo o: 20");
+//        arvore.insercaoAVL(20);
+//        arvore.printArvore();
+//        System.out.println("Inserindo o: 30");
+//        arvore.insercaoAVL(30);
+//        arvore.printArvore();
+//        System.out.println("Inserindo o: 15");
+//        arvore.insercaoAVL(15);
+//        arvore.printArvore();
     }
 }
