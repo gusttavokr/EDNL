75fddfe9390d342b52973733938d0eeaf35e0fef
gusttavokr <gustavommilitaoo@gmail.com>
2026-05-28 15:41:39 -0300

    :sparkles: feat: TAD Grafo

---
+++ Interfaces/TAD_Grafo.java
@@ -0,0 +1,35 @@
+package Interfaces;
+
+public interface TAD_Grafo {
+    // Grafo Não direcionado
+    
+    //finalVertices(a), retorna um array armazenando os vertices de uma aresta
+
+    // oposto(v, e) retorna o vertice oposto a v
+
+    // éAdjacente(v, w) retorna true se tiverem aresta ligando eles
+
+    //substituir(v, x) é um replace, vale para aresta também
+
+    // inserirVertice(o)
+    // inserirVertice(v1, v2, o) pode ter um 4 parametro, se é direcionado ou nao
+
+    // removerVertice(v), retorna
+    // removerAresta(e), retorna 
+
+    // arestasIncidentes(v), quais arestas tem esse vertice
+    // vertices() iterador com todos os vertices (bfs, dfs)
+    // arestas() 
+
+    //usar dfs
+
+    // grau
+
+
+    //  ========================== DIRECIONADO
+    // ehDirecionado(e) 
+    // inserirArestaDirecionada(v1, v2, "X")
+
+    //grau de entrada
+    //grau de saida
+}
