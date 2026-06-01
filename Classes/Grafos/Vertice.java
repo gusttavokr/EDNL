4c8d82e1ad6605c0d1246dbea8af011ea05588df
gusttavokr <gustavommilitaoo@gmail.com>
2026-06-01 10:05:21 -0300

    :sparkles: feat: getters e setters

--- Classes/Grafos/Vertice.java
+++ Classes/Grafos/Vertice.java
@@ -13,4 +13,30 @@ public class Vertice{
         this.element = x;
         this.arestas = new ArrayList<>();
     }
+
+    // GETTERS
+    public int getId(){
+        return this.id;
+    }
+    public Object element(){
+        return this.element;
+    }
+    public List<Aresta> getArestas(){
+        return this.arestas;
+    }
+
+    //SETTERS
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setElement(Object element){
+        this.element = element;
+    }
+    public void addAresta(Aresta aresta){
+        this.arestas.add(aresta);
+    }
+    public void removeAresta(Aresta aresta){
+        this.arestas.remove(aresta);
+    }
+
 }
