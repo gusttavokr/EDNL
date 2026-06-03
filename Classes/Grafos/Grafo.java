2179a22e10dd5779cf5bf65da3b32938f43f0460
gusttavokr <gustavommilitaoo@gmail.com>
2026-06-03 13:46:03 -0300

    :sparkles: feat: oposto de um vertice

--- Classes/Grafos/Grafo.java
+++ Classes/Grafos/Grafo.java
@@ -11,5 +11,21 @@ public class Grafo implements TAD_Grafo{
         return a.getVertices();
     }
 
-    
+    public Vertice oposto(Vertice v, Aresta a){
+
+        // if (a == null || v == null) {
+        //     throw new GrafoErro("Vértice ou Aresta inexistente");
+        // }
+
+        Vertice vertice1 = a.getVerticeInicio();
+        Vertice vertice2 = a.getVerticeFim();
+        
+        if (v == vertice1) {
+            return vertice2;
+        } else if (v == vertice2){
+            return vertice1;
+        }
+
+        throw new GrafoErro("O vértice: " + v + " não está na aresta.");  
+    }
 }
